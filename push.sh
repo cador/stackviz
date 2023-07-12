0177a34e2d96aecc7091d67fc935ffebf43fa7ec
youhaolin <cador.ai@aliyun.com>
2023-07-12 21:30:39 +0800

    tr

--- push.sh
+++ push.sh
@@ -1,4 +1,8 @@
 rm -rf dist
 python setup.py sdist bdist_wheel
-
 twine upload dist/*
+
+# 设置HTTP代理
+# git config --global http.proxy http://127.0.0.1:7890
+# 设置HTTPS代理
+# git config --global https.proxy https://127.0.0.1:7890
