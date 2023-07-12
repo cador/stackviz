fab3ae058049c9e51188944e33264ccac93f703d
youhaolin <cador.ai@aliyun.com>
2023-07-12 21:31:57 +0800

    Update .gitignore

--- push.sh
+++
@@ -1,8 +0,0 @@
-rm -rf dist
-python setup.py sdist bdist_wheel
-twine upload dist/*
-
-# 设置HTTP代理
-# git config --global http.proxy http://127.0.0.1:7890
-# 设置HTTPS代理
-# git config --global https.proxy https://127.0.0.1:7890
